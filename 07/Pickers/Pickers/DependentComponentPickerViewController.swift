fd71156be94da49b6382338a07f4b95ae10ab8cb
Hamidrzea Bistooni <hamidreza@hamidrzeas-mac.local>
2016-01-30 13:12:32 -0800

    create dependent tab from chapter 07 project

--- 07/Pickers/Pickers/DependentComponentPickerViewController.swift
+++ 07/Pickers/Pickers/DependentComponentPickerViewController.swift
@@ -8,13 +8,79 @@
 
 import UIKit
 
-class DependentComponentPickerViewController: UIViewController {
+class DependentComponentPickerViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+    
+    @IBOutlet weak var stateZipPicker : UIPickerView!
+    private let zipComponent = 0
+    private let stateComponent = 1
+    private var states : [String]!
+    private var zips : [String]!
+    private var statesZips : [String:[String]]!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let bundle = NSBundle.mainBundle()
+        
+        let plistUrl = bundle.URLForResource("statedictionary", withExtension: "plist")
+        
+        statesZips = NSDictionary(contentsOfURL: plistUrl!) as! [String:[String]]!
+        
+        let allStates = statesZips.keys
+        states = allStates.sort()
+        
+        let selectedZips = states[0]
+        zips = statesZips[selectedZips]
+        
+        
+        
     }
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == zipComponent {
+            return zips.count
+        } else {
+            return states.count
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == zipComponent {
+            return zips[row]
+        } else {
+            return states[row]
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == stateComponent {
+            let selectedState = states[row]
+            zips = statesZips[selectedState]
+            stateZipPicker.reloadComponent(zipComponent)
+            stateZipPicker.selectRow(0, inComponent: zipComponent,
+            animated: true)
+        }    }
+    
+    func pickerView(pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        
+        let width = pickerView.bounds.size.width
+        if component == zipComponent {
+            return width/3
+        } else {
+            return 2 * width/3
+        }
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -22,6 +88,31 @@ class DependentComponentPickerViewController: UIViewController {
     }
     
 
+    @IBAction func buttonPressed(sender: UIButton) {
+        let zipRow = stateZipPicker.selectedRowInComponent(zipComponent)
+        let stateRow = stateZipPicker.selectedRowInComponent(stateComponent)
+        
+        let zip = zips[zipRow]
+        let state = states[stateRow]
+        
+        let message = "ZIP code is : \(zip) and its for \(state) state "
+        
+        
+        let alert = UIAlertController(title: "Selected state and zip", message: message, preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        
+        alert.addAction(action)
+        
+        presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
