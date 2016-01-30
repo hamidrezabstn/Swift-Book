2858a21696205bb447b2754ecbac5196f27f7685
Hamidreza Bistooni <hamidrezabstn@gmail.com>
2016-01-30 00:32:45 -0800

    create double component actions and outlets override methods from data source and data delegate protocols

--- 07/Pickers/Pickers/DoubleComponentPickerViewController.swift
+++ 07/Pickers/Pickers/DoubleComponentPickerViewController.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
-class DoubleComponentPickerViewController: UIViewController {
+class DoubleComponentPickerViewController: UIViewController , UIPickerViewDataSource , UIPickerViewDelegate {
 
+    @IBOutlet weak var doublePicker: UIPickerView!
+    
+    private let fillingComponent = 0
+    private let breadComponent = 1
+    private let fillingTypes = [
+        "Ham", "Turkey", "Peanut Butter", "Tuna Salad",
+        "Chicken Salad", "Roast Beef", "Vegemite"]
+    private let breadTypes = [
+        "White", "Whole Wheat", "Rye", "Sourdough",
+        "Seven Grain"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +33,49 @@ class DoubleComponentPickerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Override Methods
+    // MARK: Datasource Methods
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == breadComponent {
+            return breadTypes.count
+    } else {
+            return fillingTypes.count
+        }
+    }
+    // MARK: Datadelegate Methods
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == breadComponent {
+                return breadTypes[row]
+            } else {
+                return fillingTypes[row]
+        }
+    }
+    
+    
+    @IBAction func buttonPressed(sender: UIButton) {
+        let fillingRow = doublePicker.selectedRowInComponent(fillingComponent)
+        let breadRow = doublePicker.selectedRowInComponent(breadComponent)
+        
+        let fillingValue = fillingTypes[fillingRow]
+        let breadValue = breadTypes[breadRow]
+        
+        let message = "Your \(fillingValue) on \(breadValue) bread will be right up."
+        let alert = UIAlertController(
+            title: "Thank you for your order",
+            message: message,
+            preferredStyle: .Alert)
+        let action = UIAlertAction(
+            title: "Great",
+            style: .Default,
+            handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    }
 
     /*
     // MARK: - Navigation
@@ -32,4 +87,4 @@ class DoubleComponentPickerViewController: UIViewController {
     }
     */
 
-}
+
