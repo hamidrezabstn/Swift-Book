fc114ece23c342153b6eba6d8a095b8a44ed1d0e
Hamidreza Bistooni <hamidrezabstn@gmail.com>
2016-01-29 23:19:09 -0800

    create outlet and action method for date tab finish date tab

--- 07/Pickers/Pickers/DatePickerViewController.swift
+++ 07/Pickers/Pickers/DatePickerViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class DatePickerViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let date  = NSDate()
+        
+        datePicker.setDate(date, animated: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +29,20 @@ class DatePickerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func buttonPressed(sender: UIButton) {
+        
+        let date = datePicker.date
+        let msg = "The date that you have been selected is \(date)"
+        
+        let alert  = UIAlertController(title: "Date Selected", message: msg, preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "Thanks", style: .Default, handler: nil)
+        
+        alert.addAction(action)
+        
+        presentViewController(alert, animated: true, completion: nil)
+        
+    }
 
     /*
     // MARK: - Navigation
