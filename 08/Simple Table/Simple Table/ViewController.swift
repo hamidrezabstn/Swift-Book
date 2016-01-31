6c147e96346661eff16289b10202d8416fcd6b67
Hamidreza Bistooni <hamidrezabstn@gmail.com>
2016-01-31 00:45:38 -0800

    create a simple table and provide data for it

--- 08/Simple Table/Simple Table/ViewController.swift
+++ 08/Simple Table/Simple Table/ViewController.swift
@@ -8,7 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource , UITableViewDelegate {
+    
+    private let dwarves = [
+        "Sleepy", "Sneezy", "Bashful", "Happy",
+        "Doc", "Grumpy", "Dopey",
+        "Thorin", "Dorin", "Nori", "Ori",
+        "Balin", "Dwalin", "Fili", "Kili",
+        "Oin", "Gloin", "Bifur", "Bofur",
+        "Bombur"
+    ]
+    
+    let simpleTableIdentifier = "SimpleTableIdentifier"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +31,65 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dwarves.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            var cell = tableView.dequeueReusableCellWithIdentifier(simpleTableIdentifier)
+            
+            if cell == nil {
+                cell = UITableViewCell(style: .Subtitle, reuseIdentifier: simpleTableIdentifier)
+            }
+            cell!.textLabel?.text = dwarves[indexPath.row]
+            
+            let image = UIImage(named: "star")
+            cell!.imageView?.image = image
+            
+            let imageHighlight = UIImage(named: "star2")
+            cell!.imageView?.highlightedImage = imageHighlight
+            
+            if indexPath.row > 7{
+                cell!.detailTextLabel?.text = "salam"
+            } else {
+                cell!.detailTextLabel?.text = "bye"
+            }
+            
+            return cell!
+            
+    }
+    
+    func tableView(tableView: UITableView, indentationLevelForRowAtIndexPath indexPath: NSIndexPath) -> Int {
+                return indexPath.row % 4
+    }
+    
+    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+            if indexPath.row == 0{
+                return nil
+            }else if indexPath.row % 2 == 0{
+                return NSIndexPath(forRow: indexPath.row + 1 , inSection:indexPath.section)
+            } else {
+                return indexPath
+                }
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let row = indexPath.row
+        let selectedText = dwarves[row]
+        
+        let alert = UIAlertController(title: "Selected", message: selectedText, preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+            return indexPath.row == 0 ? 120 : 70
+    }
+    
+    
 }
 
