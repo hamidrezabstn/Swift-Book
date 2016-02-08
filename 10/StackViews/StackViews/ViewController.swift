962d3c44059e31ce13082329dd00b74e34ed51f9
Hamidreza Bistooni <hamidrezabstn@gmail.com>
2016-02-08 01:29:52 -0800

    add codes for application to run finish chapter 10

--- 10/StackViews/StackViews/ViewController.swift
+++ 10/StackViews/StackViews/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private static let imageCount = 6;
+    private var index = 0
+    
+    @IBOutlet weak var imageTitle: UILabel!
+    @IBOutlet weak var imageContainer: UIImageView!
+    
+    
+    @IBAction func nextImage(sender: UIButton) {
+        index = (index + 1) % ViewController.imageCount
+        showImage(index)
+    }
+    
+    @IBAction func prevImage(sender: UIButton) {
+        index = index == 0 ? ViewController.imageCount - 1 : --index
+        showImage(index)    
+    }
+    
+    @IBAction func resetImage(sender: UIButton) {
+        showImage(0)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        showImage(0)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +43,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func showImage(index: Int) {
+        imageTitle.text = "Image \(index)"
+        imageContainer.image = UIImage(named: "image\(index)")
+    }
 
 }
 
