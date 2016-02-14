9d6cc8c6e4b5e293310bc8297d5192804b52c17f
Hamidreza Bistooni <hamidrezabstn@gmail.com>
2016-02-14 11:50:27 +0330

    create project for Persistence data in chapter 13

---
+++ 13/Persistence/Persistence/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Persistence
+//
+//  Created by Iransamaneh on 11/25/1394 AP.
+//  Copyright © 1394 Iransamaneh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
